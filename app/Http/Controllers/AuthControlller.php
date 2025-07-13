81cd330301aa022ef8e16cc2ead6cfb2cf56e273
Aadarshag7 <aadarshgautam40@gmail.com>
2025-07-13 21:40:47 +0545

    continue Auth

--- app/Http/Controllers/AuthControlller.php
+++ app/Http/Controllers/AuthControlller.php
@@ -3,11 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthControlller extends Controller
 {
-    public function register()
+    public function register(){
+        return view('register');
+    }
     
+  
+   
 
+
+
+public function registerSave(Request $request){
+
+    $validation=$request->validate([
+        'name'=>['required'],
+        'email'=>['required'],
+        'password'=>['required']
+
+    ]);
+
+    User::create([
+        'name'=>$request->name,
+        'email'=>$request->email,
+        'password'=>Hash::make($request->password)
+
+    ])
 
 }
