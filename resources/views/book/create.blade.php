5fc9a5891728b5ccbcf03d11746d8d94959851a7
Aadarshag7 <aadarshgautam40@gmail.com>
2025-07-26 22:14:21 +0545

    3rd Api

--- resources/views/book/create.blade.php
+++ resources/views/book/create.blade.php
@@ -10,7 +10,7 @@
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('book.store') }}">
-                       @csrf
+                    @csrf
 
                         <div class="row mb-3">
                             <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Full Name') }}</label>
